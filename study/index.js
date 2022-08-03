aa566b533225205fbad8805fca57f28e4e135d61
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2022-08-03 15:26:11 +0900

    feat : 개발환경 세팅

---
+++ study/index.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var sum = add(1, 2);
+console.log(sum);
---
+++ study/index.ts
@@ -0,0 +1,5 @@
+function add(a : number, b:number){
+    return a + b;
+}
+const sum: number = add(1,2);
+console.log(sum);
