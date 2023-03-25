e3c28f35195b2116d506ee90de26ed5f7737d87a
pangkyu <baesense@naver.com>
2023-03-25 12:53:21 +0900

    ts study

--- typescript-test/dist/test.js
+++ typescript-test/dist/test.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("hi ts");
+const test = (props) => {
+    // 타입을 명시하지 않은 경우기 때문에 props는 any로 추론된다.
+    return props * 3; // 리턴타입은 number로 추론된다.
+};
+// props가 any이기 때문에 아래 두 개는 에러가 발생하지 않음
+test(10); // 30
+test("plus") + 5; // NaN
+console.log(test(10));
+console.log(test("plus") + 5);
+function f4(a) {
+    if (a > 0)
+        return a * 30;
+    else
+        return a + 10;
+}
+console.log(f4(5) + 5);
+function hello(person) {
+    console.log(`안녕! ${person.name}이야 `);
+}
+const p = {
+    name: "mark",
+    age: 39,
+};
--- typescript-test/src/test.ts
+++ typescript-test/src/test.ts
@@ -1,1 +1,28 @@
-console.log("hi ts");
+const test = (props: any) => {
+  // 타입을 명시하지 않은 경우기 때문에 props는 any로 추론된다.
+  return props * 3; // 리턴타입은 number로 추론된다.
+};
+// props가 any이기 때문에 아래 두 개는 에러가 발생하지 않음
+test(10); // 30
+test("plus") + 5; // NaN
+console.log(test(10));
+console.log(test("plus") + 5);
+
+function f4(a: number): number {
+  if (a > 0) return a * 30;
+  else return a + 10;
+}
+console.log(f4(5) + 5);
+
+interface Person1 {
+  name: string;
+  age: number;
+}
+
+function hello(person: { name: string; age: number }): void {
+  console.log(`안녕! ${person.name}이야 `);
+}
+const p: Person1 = {
+  name: "mark",
+  age: 39,
+};
